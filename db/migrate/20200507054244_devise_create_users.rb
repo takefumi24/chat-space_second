da67029216b3edd27e8ddf4dc4da7f9b8c4eabb0
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-07 15:02:07 +0900

    Userモデル作成2

--- db/migrate/20200507054244_devise_create_users.rb
+++ db/migrate/20200507054244_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -35,7 +36,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
       t.timestamps null: false
     end
-
+    add_index :users, :name,                 unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
