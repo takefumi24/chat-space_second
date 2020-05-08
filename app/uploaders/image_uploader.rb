1a709398414eb7a7c5ea8b37f46989216e6a346a
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-08 17:36:58 +0900

    ImageUpladerの画像の大きさ変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -45,6 +45,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-    process resize_to_fit: [800, 800]
+    process resize_to_fit: [300, 200]
 
 end
