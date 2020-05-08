5d9e2e70b8e98cf31275772e86198dd1d1340fe2
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-08 13:34:59 +0900

    トップページの表示を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
