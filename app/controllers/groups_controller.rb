e1575e9f3df89e9743b03b418fa80f879f9ab6db
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-08 12:50:38 +0900

    groupsの保存機能を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,20 @@
 class GroupsController < ApplicationController
-   def new
-   end
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
 
-   def create
-   end
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
 end
