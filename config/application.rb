884a201ceffeb593646b0bbb95bc09b1570265e6
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-09 01:15:02 +0900

    Railsのバリデーションエラーで、「field_with_errors」によるレイアウト崩れを防ぐ

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module ChatSpaceSecond
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
