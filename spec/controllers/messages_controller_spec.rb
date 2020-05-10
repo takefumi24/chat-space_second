59f53aafacfb958b1b9d25b60c19a733a86be05c
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-10 14:02:34 +0900

    messagesコントローラでメッセージを作成するアクションのテスト

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -33,4 +33,58 @@ describe MessagesController do
         expect(response).to redirect_to(new_user_session_path)
       end
     end
+
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+    context 'ログインしている場合' do
+    # この中にログインしている場合のテストを記述
+      before do
+        login user
+      end
+
+      context '保存に成功した場合' do
+      # この中にメッセージの保存に成功した場合のテストを記述
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'messageを保存すること' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'group_messages_pathへリダイレクトすること' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context '保存に失敗した場合' do
+      # この中にメッセージの保存に失敗した場合のテストを記述
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+
+          subject {
+            post :create,
+            params: invalid_params
+          }
+
+        it 'messageを保存しないこと' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'index.html.erbへ遷移すること' do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    context 'ログインしていない場合' do
+    # この中にログインしていない場合のテストを記述
+      it ' new.html.hamlに遷移すること' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
