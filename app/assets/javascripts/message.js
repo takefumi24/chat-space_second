99487964825961c99374fefe51e48b3dc9a177a5
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-18 14:40:46 +0900

    返ってきたJSONをdoneメソッドで受取り、HTMLを形成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,43 @@
 $(function () {
+  function buildHtml(message) {
+    if (message.image) {
+      const html =`<div class="message">
+                    <div class="message__info">
+                      <p class="message__info--name">
+                        ${message.user_name}
+                      </p>
+                      <p class="message__info--data">
+                        ${message.created_at}
+                      </p>
+                    </div>
+                      <p class="message__text">
+                        ${message.content}
+                      </p>
+                    <img class="lower-message__image" src=${message.image}
+                    </div>`
+      return html
+    } else {
+      const html = `<div class="message">
+                    <div class="message__info">
+                      <p class="message__info--name">
+                        ${message.user_name}
+                      </p>
+                      <p class="message__info--data">
+                        ${message.created_at}
+                      </p>
+                    </div>
+                      <p class="message__text">
+                        ${message.content}
+                      </p>
+                    </div>`
+      return html
+    };
+  }
+
+
+
+
+
   $('#new_message').on('submit', function (e) {
     e.preventDefault();
     const formData = new FormData(this);
@@ -10,6 +49,10 @@ $(function () {
       datatype: 'json',
       processData: false,
       contentType: false
+    })
+      .done(function (data) {
+        const html = buildHtml(data);
+
     })
   });
 });
